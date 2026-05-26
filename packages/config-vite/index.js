c04f6f992a292e4ebb0c311c8cbe126144b18c34
Miloš Nikolić <nikolic.milos86@gmail.com>
2026-05-26 11:53:42 +0200

    Fix pixi-svelte build: use conditional plugin imports in config-vite

--- packages/config-vite/index.js
+++ packages/config-vite/index.js
@@ -1,14 +1,31 @@
 // Don't convert this to a ts file, because of this https://github.com/vitejs/vite/issues/5370
-import { sveltekit } from '@sveltejs/kit/vite';
-import { lingui } from '@lingui/vite-plugin';
 import { defineConfig } from 'vite';
+import { existsSync } from 'node:fs';
+import { resolve } from 'node:path';
 
 const NODE_ENV = process.env.NODE_ENV;
 let dev = NODE_ENV === 'development';
+const hasLinguiConfig = () =>
+	['lingui.config.js', 'lingui.config.cjs', 'lingui.config.mjs', 'lingui.config.ts'].some(
+		(fileName) => existsSync(resolve(process.cwd(), fileName)),
+	);
+const hasSvelteKitApp = () => existsSync(resolve(process.cwd(), 'src/app.html'));
 
-export default () =>
-	defineConfig({
-		plugins: [sveltekit(), lingui()],
+export default async () => {
+	const plugins = [];
+
+	if (hasSvelteKitApp()) {
+		const { sveltekit } = await import('@sveltejs/kit/vite');
+		plugins.push(sveltekit());
+	}
+
+	if (hasLinguiConfig()) {
+		const { lingui } = await import('@lingui/vite-plugin');
+		plugins.push(lingui());
+	}
+
+	return defineConfig({
+		plugins,
 		logLevel: 'info',
 		build: {
 			assetsInlineLimit: Infinity,
@@ -25,3 +42,4 @@ export default () =>
 			},
 		},
 	});
+};
